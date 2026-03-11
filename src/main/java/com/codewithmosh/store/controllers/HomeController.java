282205a965ddc5d962ca12aba5e63c7a266b472b
Avotriniaina Ras <avotriniainaras2@gmail.com>
2026-03-11 10:46:49 +0300

    feat add thymeleaf to the project

--- src/main/java/com/codewithmosh/store/controllers/HomeController.java
+++ src/main/java/com/codewithmosh/store/controllers/HomeController.java
@@ -2,16 +2,18 @@ package com.codewithmosh.store.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
     @RequestMapping("/") //this annotation maps the root URL ("/") to the index() method    
-    public String index() { //the wiew name is index.html, but we don't have to specify the .html extension because Spring Boot will automatically look for it in the resources/templates folder
+    public String index(Model model) { //the wiew name is index.html, but we don't have to specify the .html extension because Spring Boot will automatically look for it in the resources/templates folder
+        model.addAttribute("name", "Avotra");
         return "index";
     }
 
-    @RequestMapping("/sayhello") 
-    public String sayHello() { //the wiew name is index.html, but we don't have to specify the .html extension because Spring Boot will automatically look for it in the resources/templates folder
-        return "index"; //t
-    }
+    // @RequestMapping("/sayhello") 
+    // public String sayHello() { //the wiew name is index.html, but we don't have to specify the .html extension because Spring Boot will automatically look for it in the resources/templates folder
+    //     return "index"; //t
+    // }
 }
